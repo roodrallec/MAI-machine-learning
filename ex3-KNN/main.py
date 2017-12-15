21d6fc0b74e2790832677fb3009fd975513922ce
Jorge Alexander <jorge@maiv.co.uk>
2017-12-15 02:01:21 +0100

    friedman accuracies for comparison against all dist_metrics, k_values and datasets

--- ex3-KNN/main.py
+++ ex3-KNN/main.py
@@ -48,7 +48,9 @@ def run_knn(knnAlgorithm, X_train, y_train, X_test):
 
 data_sets = [{'name': "hepatitis", 'dummy_value': "?", 'class_field': "Class"}]
 # ,{'name': "pen-based", 'dummy_value': "", 'class_field': "a17"}]
-results = pd.DataFrame(columns=['dataset', 'fold', 'dist_metric', 'k_value', 'run_time', 'c_matrix'])
+dist_metrics = ['euclidean', 'cosine', 'hamming', 'minkowski', 'correlation']
+k_values = [1, 3, 5, 7]
+results = pd.DataFrame(columns=['dataset', 'fold', 'dist_metric', 'k_value', 'run_time', 'c_matrix', 'accuracy'])
 
 for dataset in data_sets:
 
@@ -56,9 +58,9 @@ for dataset in data_sets:
         path = 'datasets/{0}/{0}.fold.00000{1}'.format(dataset['name'], f)
         X_train, y_train, X_test, y_test = norm_train_test_split(path, dataset['class_field'], dataset['dummy_value'])
 
-        for dist in ['euclidean', 'cosine', 'hamming', 'minkowski', 'correlation']:
+        for dist in dist_metrics:
 
-            for k in [1, 3, 5, 7]:
+            for k in k_values:
                 algo = kNNAlgorithm(k, metric=dist, p=4, policy='voting', weights=None, selection=None)
                 y_pred, delta = run_knn(algo, X_train, y_train, X_test)
                 c_matrix = confusion_matrix(y_test, y_pred)
@@ -75,13 +77,7 @@ for dataset in data_sets:
     the second best rank 2. . . . In case of ties (like in iris, lung cancer, mushroom and primary 
     tumor), average ranks are assigned.
 """
-perfA = results[(results['dataset'] == 'hepatitis') & (results['dist_metric'] == 'cosine') & (results['k_value'] == 3)]
-perfB = results[(results['dataset'] == 'hepatitis') & (results['dist_metric'] == 'cosine') & (results['k_value'] == 5)]
-perfC = results[(results['dataset'] == 'hepatitis') & (results['dist_metric'] == 'cosine') & (results['k_value'] == 7)]
-friedman_chi, p_value = friedmanchisquare(list(perfA['accuracy']), list(perfB['accuracy']), list(perfC['accuracy']))
-
-fold_average = results.groupby(['dataset', 'dist_metric', 'k_value']).agg({'accuracy': np.mean})
-print(fold_average)
-
-
+sorted_results = results.sort_values(['dataset', 'dist_metric', 'k_value'], ascending=[1,1,1])
+grouped_accuracies = np.array_split(sorted_results['accuracy'], len(data_sets)*len(k_values)*len(dist_metrics))
+friedman_chi, p_value = friedmanchisquare(*grouped_accuracies)
 

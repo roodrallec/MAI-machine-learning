9a6643e168a2a26774600f1f7e6c804ce5733005
emerrf <emer@emer.es>
2017-12-19 11:17:10 +0100

    Adding Ex4 definition

---
+++ ex4-SVM/exercise1_svm.py
@@ -0,0 +1,119 @@
+﻿#############################################################
+#############################################################
+#############################################################
+
+
+import numpy as np
+import cvxopt
+import cvxopt.solvers
+from sklearn.svm import SVC
+import matplotlib.pyplot as plt
+
+
+if __name__ == "__main__":
+    import pylab as pl
+
+    def generate_data_set1():
+        # generate training data in the 2-d case
+        mean1 = np.array([0, 2])
+        mean2 = np.array([2, 0])
+        cov = np.array([[0.8, 0.6], [0.6, 0.8]])
+        X1 = np.random.multivariate_normal(mean1, cov, 100)
+        y1 = np.ones(len(X1))
+        X2 = np.random.multivariate_normal(mean2, cov, 100)
+        y2 = np.ones(len(X2)) * -1
+        return X1, y1, X2, y2
+
+    def generate_data_set2():
+        mean1 = [-1, 2]
+        mean2 = [1, -1]
+        mean3 = [4, -4]
+        mean4 = [-4, 4]
+        cov = [[1.0,0.8], [0.8, 1.0]]
+        X1 = np.random.multivariate_normal(mean1, cov, 50)
+        X1 = np.vstack((X1, np.random.multivariate_normal(mean3, cov, 50)))
+        y1 = np.ones(len(X1))
+        X2 = np.random.multivariate_normal(mean2, cov, 50)
+        X2 = np.vstack((X2, np.random.multivariate_normal(mean4, cov, 50)))
+        y2 = np.ones(len(X2)) * -1
+        return X1, y1, X2, y2
+
+    def generate_data_set3():
+        # generate training data in the 2-d case
+        mean1 = np.array([0, 2])
+        mean2 = np.array([2, 0])
+        cov = np.array([[1.5, 1.0], [1.0, 1.5]])
+        X1 = np.random.multivariate_normal(mean1, cov, 100)
+        y1 = np.ones(len(X1))
+        X2 = np.random.multivariate_normal(mean2, cov, 100)
+        y2 = np.ones(len(X2)) * -1
+        return X1, y1, X2, y2
+
+    def split_train(X1, y1, X2, y2):
+        X1_train = X1[:90]
+        y1_train = y1[:90]
+        X2_train = X2[:90]
+        y2_train = y2[:90]
+        X_train = np.vstack((X1_train, X2_train))
+        y_train = np.hstack((y1_train, y2_train))
+        return X_train, y_train
+
+    def split_test(X1, y1, X2, y2):
+        X1_test = X1[90:]
+        y1_test = y1[90:]
+        X2_test = X2[90:]
+        y2_test = y2[90:]
+        X_test = np.vstack((X1_test, X2_test))
+        y_test = np.hstack((y1_test, y2_test))
+        return X_test, y_test
+
+    def run_svm_dataset1():
+        X1, y1, X2, y2 = generate_data_set1()
+        X_train, y_train = split_train(X1, y1, X2, y2)
+        X_test, y_test = split_test(X1, y1, X2, y2)
+
+         #### 
+        # Write here your SVM code and choose a linear kernel
+        # plot the graph with the support_vectors_
+        # print on the console the number of correct predictions and the total of predictions
+        ####
+
+
+    def run_svm_dataset2():
+        X1, y1, X2, y2 = generate_data_set2()
+        X_train, y_train = split_train(X1, y1, X2, y2)
+        X_test, y_test = split_test(X1, y1, X2, y2)
+
+        #### 
+        # Write here your SVM code and choose a linear kernel with the best C pparameter
+        # plot the graph with the support_vectors_
+        # print on the console the number of correct predictions and the total of predictions
+        ####
+
+
+
+    def run_svm_dataset3():
+        X1, y1, X2, y2 = generate_data_set3()
+        X_train, y_train = split_train(X1, y1, X2, y2)
+        X_test, y_test = split_test(X1, y1, X2, y2)
+
+        #### 
+        # Write here your SVM code and use a gaussian kernel 
+        # plot the graph with the support_vectors_
+        # print on the console the number of correct predictions and the total of predictions
+        ####
+
+
+
+#############################################################
+#############################################################
+#############################################################
+
+# EXECUTE SVM with THIS DATASETS      
+    run_svm_dataset1()   # data distribution 1
+    run_svm_dataset2()   # data distribution 2
+    run_svm_dataset3()   # data distribution 3
+
+#############################################################
+#############################################################
+#############################################################
